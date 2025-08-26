ecd41c6b03acd6675d851d55d3dbf8ad629b8169
lind <trananhtu8900@gmail.com>
2025-08-26 15:43:56 +0700

    commit code loop form + start + end

--- app/components/form/page.tsx
+++ app/components/form/page.tsx
@@ -1,45 +1,193 @@
 "use client";
 
 import React, { forwardRef, useEffect, useImperativeHandle } from 'react';
-import { Drawer, Loader } from "@mantine/core";
+import { Box, Button, Drawer, Group, Text, TextInput } from "@mantine/core";
 import { useState } from "react";
 import { IconXboxX } from '@tabler/icons-react';
+import Image from "next/image";
+
+import UserTask from "../../../public/icon/user-task.svg";
+import SendEmail from "../../../public/icon/email-task.svg";
+import SendHTTP from "../../../public/icon/http-request-task.svg";
+import Organization from "../../../public/icon/organization-task.svg";
+import Notification from "../../../public/icon/notification-task.svg";
+import Record from "../../../public/icon/record-task.svg";
+import GetRecord from "../../../public/icon/get-record-task.svg";
+import Loop from "../../../public/icon/loop-task.svg";
+import PhoneCall from "../../../public/icon/phone-call-task.svg";
+
+import Exclusive from "../../../public/icon/exclusive-gateway.svg";
+import Inclusive from "../../../public/icon/inclusive-gateway.svg";
+import Parallel from "../../../public/icon/parallel-gateway.svg";
+
+import Wait from "../../../public/icon/wait-task.svg";
+import Ends from "../../../public/icon/end-event.svg";
+import Start from "../../../public/icon/start-event.svg";
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import "./css/style.css";
+import LoopForm from './content-form/loop-form';
 
 type GoFormProps = {
-    elementProp: any; 
+    elementProp: any;
 };
 
 // Kiểu cho ref (những gì bạn expose ra ngoài)
 export type GoFormRef = {
-  openModal: () => void;
+    openModal: () => void;
 };
 
 
 const GoForm = forwardRef<GoFormRef, GoFormProps>((props, ref) => {
-     const { elementProp } = props;
+    const { elementProp } = props;
     const [opened, setOpened] = useState(false);
-    const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState<string>("");
+    const [imgIcon, setImgIcon] = useState<StaticImport | string>("");
+    const [submitTrigger, setSubmitTrigger] = useState(false);
+
+    const [value, setValue] = useState("");
+    const maxLength = 255;
     useImperativeHandle(ref, () => ({
         openModal: () => {
             setOpened(true)
         }
     }));
+    useEffect(() => {
+        getTitle(elementProp?.type);
+    }, [elementProp?.type])
+
+    const getTitle = (type: string) => {
+        switch (type) {
+            case "bpmn:SendTask":
+                setTitle("Send mail");
+                setImgIcon(SendEmail);
+                break;
+            case "bpmn:UserTask":
+                setTitle("Use task")
+                setImgIcon(UserTask);
+                break;
+            case "bpmn:ExclusiveGateway":
+                setTitle("Exclusive Gateway");
+                setImgIcon(Exclusive);
+                break;
+            case "elEx:HttpTask":
+                setImgIcon(SendHTTP);
+                setTitle("Send HTTP Request");
+                break;
+            case "elEx:OrganizationTask":
+                setImgIcon(Organization);
+                setTitle("Origanzation")
+                break;
+            case "elEx:SendNotificationTask":
+                setImgIcon(Notification);
+                setTitle("Send Notification")
+                break;
+            case "elEx:CreateRecordTask":
+                setImgIcon(Record);
+                setTitle("Crate and Update Record");
+                break;
+            case "elEx:GetRecordTask":
+                setImgIcon(GetRecord);
+                setTitle("Get Record'")
+                break;
+            case "elEx:LoopTask":
+                setImgIcon(Loop);
+                setTitle("Loop")
+                break;
+            case "bpmn:ServiceTask":
+                setImgIcon(PhoneCall);
+                setTitle("Phone Call");
+                break;
+            case "bpmn:InclusiveGateway":
+                setImgIcon(Inclusive);
+                setTitle("Inclusive gateway")
+                break;
+            case "bpmn:ParallelGateway":
+                setImgIcon(Parallel);
+                setTitle("Parallel Gateway")
+                break;
+            case "elEx:WaitTask":
+                setImgIcon(Wait);
+                setTitle("Wait")
+                break;
+            case "bpmn:EndEvent":
+                setImgIcon(Ends);
+                setTitle("Ends")
+                break;
+            default:
+                setImgIcon(Start);
+                setTitle("Start");
+        }
+    }
+
+
     return <>
-        <Drawer className={'mantine-Drawer-prmu'} size={'60%'} opened={opened} position={'right'} onClose={() => {
+        <Drawer className={'mantine-Drawer-prmu'} title={
+            <Group gap="sm" wrap="nowrap">
+                <Image
+                    src={imgIcon}
+                    alt=""
+                    className="w-7 h-7 "
+                />
+                <div>
+                    <Text c="dark" fw={700} fz="lg">{title}</Text>
+                </div>
+            </Group>
+        } size={'55%'} opened={opened} position={'right'} onClose={() => {
             setOpened(false)
         }}
             closeButtonProps={{
-                icon: <IconXboxX size={40} stroke={2} style={{ color: "#f90101ff" }} />,
+                icon: <IconXboxX size={40} stroke={2} style={{ color: "#000000ff" }} />,
             }}
         >
 
             <div className="bg-white rounded-2xl contents justify-center px-6 pb-6 w-full max-w-md relative">
-                {loading == true && <Loader color="blue" type="dots" />}
-                <h2 className="text-center text-xl font-bold text-blue-600 mb-2">Premium Plans</h2>
-                <p className="text-center text-sm text-gray-500 font-semibold mb-4">Access to more days in the
-                    past<br />More favorites + personal notes!</p>
-                <span>{JSON.stringify(elementProp)}</span>
 
+                {(elementProp?.type === "bpmn:EndEvent" || elementProp?.type === "bpmn:StartEvent")
+                    &&
+                    <TextInput
+                        label={
+                            <span className='text-black'>
+                                Tên <span style={{ color: "red" }}>*</span>
+                            </span>
+                        }
+                        placeholder="Nhập tên..."
+                        value={value}
+                        onChange={(event) => setValue(event.currentTarget.value)}
+                        maxLength={maxLength}
+                        // ✅ Thêm counter bên phải input
+                        rightSection={
+                            <Text size="xs" c="dimmed">
+                                {value.length}/{maxLength}
+                            </Text>
+                        }
+                        rightSectionWidth={65}
+                        styles={{
+                            input: {
+                                backgroundColor: "#fafbff", // màu giống ảnh
+                                paddingRight: 60,
+                            },
+                        }}
+                    />}
+
+                {elementProp?.type === "elEx:LoopTask" && <LoopForm submitTrigger={submitTrigger} onSubmit={(data) => console.log(data)}/>}
+                <Box
+                    pos="fixed"
+                    bottom={0}
+                    left={0}
+                    right={0}
+                    style={{
+                        borderTop: "1px solid var(--mantine-color-gray-3)",
+                        background: "white",
+                        padding: "12px 16px",
+                    }}
+                >
+                    <Group justify="flex-end">
+                        <Button variant="default" onClick={() => setOpened(false)}>
+                            Hủy
+                        </Button>
+                        <Button onClick={() => setSubmitTrigger((prev) => !prev)}>Hoàn thành</Button>
+                    </Group>
+                </Box>
             </div>
         </Drawer>
     </>
--- app/types/consts/index.ts
+++ app/types/consts/index.ts
@@ -30,3 +30,8 @@ export interface BpmnElement {
   width: number;
   height: number;
 }
+
+export type childProps = {
+  submitTrigger: boolean;
+  onSubmit: (data: any) => void;
+}
