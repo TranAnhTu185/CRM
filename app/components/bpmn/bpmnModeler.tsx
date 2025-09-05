50b2816d72a8f9dab368ef2f9239fdfcf4b443f3
lind <trananhtu8900@gmail.com>
2025-09-05 16:56:17 +0700

    commit

--- app/components/bpmn/bpmnModeler.tsx
+++ app/components/bpmn/bpmnModeler.tsx
@@ -167,6 +167,120 @@ export default function BpmnCanvas({
     }
   };
 
+  if (modeler) {
+    //event connect node when clik
+    modeler.get('eventBus').off('element.click'); // clear trước để tránh double bind
+    if (connectMode) {
+      modeler.get('eventBus').on('element.click', (e: any) => {
+        const element = e.element;
+        // bỏ qua click vào diagram/background
+        if (!element || element.type === 'bpmn:Process') return;
+
+        const modeling = modeler!.get('modeling');
+        const elementRegistry = modelerRef.current!.get('elementRegistry');
+        const source = elementRegistry.get(connectingNode);
+        modeling.connect(source, element, {
+          type: 'bpmn:SequenceFlow',
+        });
+        setConnectingNode(null); // reset sau khi nối xong
+        setConnectMode(false); // tắt connect mode
+        removeOutline();
+      });
+
+      modeler.get('eventBus').on('connect.end', (e: any) => {
+        const { source, target, connection } = e.context;
+
+        if (target) {
+          removeOutline();
+        } else {
+          console.log('❌ Thả vào chỗ trống, không tạo connection');
+        }
+      });
+
+    } else {
+      modeler.get('eventBus').on('element.click', (e: any) => {
+        const element = e.element;
+        setConnectingNode(element);
+        const overlays = modeler.get('overlays');
+        const elementRegistry = modeler.get('elementRegistry');
+        elementRegistry.getAll().forEach((el: any) => {
+          if (el !== element) {
+            const gfx = elementRegistry.getGraphics(el);
+            gfx.classList.remove('custom-highlight');
+          }
+        });
+        overlays.clear();
+
+        if (element) {
+          if (element.type !== "bpmn:Process") {
+            const gfx = elementRegistry.getGraphics(element);
+            if (gfx.classList.contains('custom-highlight')) {
+              gfx.classList.remove('custom-highlight');
+              overlays.clear();
+            } else {
+              gfx.classList.add('custom-highlight');
+              const target = element.labelTarget || element;
+
+              const size = 64;
+
+              const positions = [
+                { top: -12, left: size / 2 - 6 },   // top-center
+                { left: -12, top: size / 2 - 6 },   // middle-left
+                { top: size - 2, left: size / 2 - 6 }, // bottom-center
+                { left: size - 2, top: size / 2 - 6 }  // middle-right
+              ];
+              positions.forEach(pos => {
+                overlays.add(target, {
+                  position: pos,
+                  html: (() => {
+                    const dot = document.createElement('div');
+                    dot.classList.add('custom-resizer');
+                    dot.addEventListener('click', (ev) => {
+                      ev.stopPropagation();
+                      setConnectMode(true);
+                      const connect = modeler.get('connect');
+                      connect.start(e.originalEvent, element);
+                    });
+
+                    return dot;
+                  })()
+                });
+              });
+            }
+          }
+        }
+
+      });
+    }
+
+    const eventBus = modeler.get('eventBus');
+    //event double click node
+    eventBus.on('element.dblclick', function (event: any) {
+      const element = event.element; // node được double click
+
+      let label: string | undefined;
+
+      // Nếu node có name (thường dùng nhất)
+      if (element.businessObject?.name) {
+        label = element.businessObject.name;
+      }
+
+      // Nếu là label element riêng
+      if (!label && element.labels?.length > 0) {
+        label = element.labels[0].businessObject.text;
+      }
+
+      console.log("Double clicked:", element.id, "Label:", label);
+      setElementSec({ ...element, label });
+      if (goForm.current) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        goForm.current.openModal();
+      }
+    });
+
+  }
+
   // Cập nhật label node sau khi submit form
   const handleSubmitFromDrawer = (values: any) => {
     console.log('andn')
